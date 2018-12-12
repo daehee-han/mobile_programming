a4bb956f26c186126c8e999d99f086766fab9358
kmucs <rlaalswn3282@naver.com>
2018-12-12 23:12:34 +0900

    first commit

---
+++ app/src/main/java/com/kmu/ndhs/font.java
@@ -0,0 +1,30 @@
+package com.kmu.ndhs;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class font extends AppCompatTextView {
+
+    public font(Context context) {
+        super(context,null);
+        applyTypeface(context);
+    }
+
+    public font(Context context, AttributeSet attrs) {
+        super(context, attrs, android.R.attr.textViewStyle);
+        applyTypeface(context);
+    }
+
+    public font(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        applyTypeface(context);
+    }
+
+    private void applyTypeface(Context context){
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "Flower_Road.otf");
+        setTypeface(typeface);
+    }
+
+}
