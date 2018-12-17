921da95de3ec883f9e42e3acd2d791698c3aa72d
kmucs <rlaalswn3282@naver.com>
2018-12-17 18:22:08 +0900

    login screen

--- app/src/main/java/com/kmu/ndhs/MainActivity.java
+++ app/src/main/java/com/kmu/ndhs/MainActivity.java
@@ -1,7 +1,9 @@
+
 package com.kmu.ndhs;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
     }
 }
